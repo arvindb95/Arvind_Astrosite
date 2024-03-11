9f6e645e2f6d45376267e0358c261cfc7d46859f
Arvind Balasubramanian <arvind6895@gmail.com>
2024-03-11 16:37:11 +0530

    Added new favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import icon from "astro-icon";
 
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), icon()],
